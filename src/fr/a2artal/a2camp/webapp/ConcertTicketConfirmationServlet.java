ed8806cc86191022fdf0985c8025549f4e340c9b
ncunin <nicolas.cunin@a2-artal.fr>
2011-04-12 19:56:56 +0200

    useless import removed

--- src/fr/a2artal/a2camp/webapp/ConcertTicketConfirmationServlet.java
+++ src/fr/a2artal/a2camp/webapp/ConcertTicketConfirmationServlet.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.amazonaws.services.simpledb.util.SimpleDBUtils;
-
 /**
  * Servlet implementation class ConcertTicketConfirmationServlet
  */
